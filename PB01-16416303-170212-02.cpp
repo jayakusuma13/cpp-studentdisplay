52e7386560602509f23e80ee1153ac9ea78d8b7f
Jaya Kusuma <kusumavictor@yahoo.co.id>
2017-02-26 21:40:24 +0700

    Update PB01-16416303-170212-02.cpp

--- PB01-16416303-170212-02.cpp
+++ PB01-16416303-170212-02.cpp
@@ -1,25 +1,22 @@
-// NIM/Nama : 16416303
-// Nama file: PB01-16416303-170212-02.cpp
-// Tanggal  : 12 Febuari 2017
-// Deskripsi: program input data mahasiswa
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main(){
     typedef struct{
-        string nama;
-        string nim;
+        string name;
+        string id;
     }user;
-    user jaya;
+    user student;
 
-    //input nama dan nim
-    cout << "masukkan nama: ";
-    cin >> jaya.nama;
-    cout << "masukkan nim: ";
-    cin >> jaya.nim;
-    //output nama dan nim
-    cout << jaya.nama << " adalah mahasiswa ITB dengan NIM " << jaya.nim;
+    //name and id input
+    cout << "insert your name: ";
+    cin >> student.name;
+    cout << "insert your id: ";
+    cin >> student.id;
+    
+    //name and id output
+    cout << student.name << " is a student with ID: " << student.id;
 
     return 0;
 }
